d673b45b1f802044d6dc6751dfd6debfcef46617
Gaymard <33988017+Erffa@users.noreply.github.com>
2021-01-27 23:14:55 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,15 @@
+# IMPORTS
+# file system exploration / management
+import os
+# load the dataset
+import torchvision
+# image processing
+import cv2
+import numpy as np
+# to read/parse xml files
+from collections import OrderedDict
+import json
+import xmltodict
+# nice displays
+from matplotlib import pyplot as plt
+from tqdm.notebook import tqdm
